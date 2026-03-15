752e02dabbd7da04a11348c575740e589ef7012a
Ilya Gazman <ilya@gazman.dev>
2026-03-15 14:37:12 -0400

    Fix install wrapper temp file handling

--- install.sh
+++ install.sh
@@ -6,7 +6,9 @@ REPO="tele-cli"
 BRANCH="${TELE_CLI_INSTALL_BRANCH:-master}"
 SETUP_URL="https://raw.githubusercontent.com/${OWNER}/${REPO}/${BRANCH}/setup.sh"
 CACHE_BUSTER="$(date +%s)-$$"
-TMP_SCRIPT="$(mktemp "${TMPDIR:-/tmp}/tele-cli-setup.XXXXXX.sh")"
+TMP_BASE="${TMPDIR:-/tmp}"
+TMP_BASE="${TMP_BASE%/}"
+TMP_SCRIPT="$(mktemp "${TMP_BASE}/tele-cli-setup.XXXXXX")"
 
 cleanup() {
   rm -f "$TMP_SCRIPT"
